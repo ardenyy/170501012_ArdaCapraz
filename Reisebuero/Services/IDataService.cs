06467e7791e5af390cb1a18e21bc409ecbd2d46b
Arda Çapraz <ardacapraz@gmail.com>
2022-04-26 15:10:20 +0300

    Created GenericDataService for CRUD Operations

---
+++ Reisebuero/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Reisebuero.Services
+{
+    public interface IDataService<T>
+    {
+        Task<T> Create(T entity);
+        Task Delete(int id);
+        Task<T> Update(int id, T entity);
+        Task<T?> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+    }
+}
