0a8e41cb1792807e65e7a449231dff28482fa545
Arda Çapraz <ardacapraz@gmail.com>
2022-04-26 15:08:57 +0300

    Added Db Context for EntityFramework and SQL Server, Added settings for connection string

---
+++ Reisebuero/Services/ReisebueroDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Reisebuero.Models;
+
+namespace Reisebuero.Services
+{
+    public class ReisebueroDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<LoginForm> LoginForms { get; set; }
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<TourSale> TourSales { get; set; }
+
+        public ReisebueroDbContext(DbContextOptions options) : base(options) { }
+    }
+}
---
+++ Reisebuero/Services/ReisebueroDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Reisebuero.Services
+{
+    public class ReisebueroDbContextFactory : IDesignTimeDbContextFactory<ReisebueroDbContext>
+    {
+        public ReisebueroDbContext CreateDbContext(string[] args = null)
+        {
+            var options = new DbContextOptionsBuilder<ReisebueroDbContext>();
+            //options.UseSqlServer(ConfigurationManager.ConnectionStrings["ReisebueroDB"].ConnectionString);
+            //options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ReisebueroDB;Integrated Security=True");
+            options.UseSqlServer(Properties.Settings.Default.ConnectionString);
+            return new ReisebueroDbContext(options.Options);
+        }
+    }
+}
