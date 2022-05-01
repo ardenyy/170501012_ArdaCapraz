8b15448372c41cdcfc23a17476f4f0c864e16063
Arda Çapraz <ardacapraz@gmail.com>
2022-05-02 00:32:12 +0300

    Added Admin user creation on first startup

--- Reisebuero/App.xaml.cs
+++ Reisebuero/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Reisebuero.Models;
+using Reisebuero.Services;
 using System.Windows;
 
 namespace Reisebuero
@@ -13,5 +9,34 @@ namespace Reisebuero
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            CreateAdminIfNotExist();
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Creates admin account on first run of application with id = 0, password = admin.
+        /// </summary>
+        /// <returns></returns>
+        private async void CreateAdminIfNotExist()
+        {
+            GenericDataService<Employee> employeeService = new GenericDataService<Employee>(new ReisebueroDbContextFactory());
+            GenericDataService<LoginForm> loginService = new GenericDataService<LoginForm>(new ReisebueroDbContextFactory());
+            Employee? returnedEmployee = await employeeService.Get(0);
+            if (returnedEmployee == null)
+            {
+                Employee employee = new Employee();
+                employee.ID = 0;
+                employee.Name = "admin";
+                employee.Surname = "admin";
+                employee.Role = Utilities.Constants.Role.Administrator;
+                LoginForm loginForm = new LoginForm();
+                loginForm.ID = employee.ID;
+                loginForm.Password = "admin";
+                _ = await employeeService.Create(employee);
+                _ = await loginService.Create(loginForm);
+            }
+        }
     }
 }
